c7eed308a3b5de8c521109865abc496d14672a27
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-30 21:15:38 +0530

    Greetings !!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Employee;
+
+namespace Emloyee
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program \n");
+            
+        }
+    }
+}
