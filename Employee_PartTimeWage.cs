8b5487eb49b0a3c07b39b3b9cae0bbb9db765846
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-31 17:32:19 +0530

    Calculating Part time Wage of Employee

---
+++ Employee_PartTimeWage.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmplyOops
+{
+    public class EmployeeCal_PartTimeWage
+    {
+        public  void Cal_Pwage()
+        {
+            Console.WriteLine("Program for Calculating Employee Wage !!");
+
+            int wagePerHour = 20;
+            int workingHours = 8;
+            int Totalwork = 0;
+            int Employeee = 1;
+            int atten = EmployeeAttendance.checkatt(Employeee);
+
+            if (atten == 1)
+            {
+                //Console.WriteLine("\n employee is present");
+                Totalwork = wagePerHour * workingHours;
+                Console.WriteLine("total work : " + Totalwork);
+            }
+            else if (atten == 2)
+            {
+                Totalwork = wagePerHour * (workingHours / 2);
+                Console.WriteLine("total work : " + Totalwork);
+            }
+            else
+            {
+                //Console.WriteLine("\n employee is absent");
+                Console.WriteLine("total work : " + Totalwork);
+            }
+        }
+    }
+}
