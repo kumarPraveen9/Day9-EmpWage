660f9a65864e83fd9ac27a052206c7b2561dde46
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-03 16:43:01 +0530

    Refactoring code with Oops method and objects

--- RefactoringCode.cs
+++ RefactoringCode.cs
@@ -8,40 +8,48 @@ namespace EmplyOops
 {
     public class RefactoringCode
     {
-        public void Cal_Wage(int maxiDays,int maxiHours)
+        public static void Cal_Wage()
         {
             Console.WriteLine("Program for Calculating Employee Wage !!");
 
-            int workingHours ;
-            int Employeee = 1;
+            int workingHours=8 ;
+           
             int emphours = 0;
             int days = 1;
-            int maxdays = maxiDays;
-            int maxhours = maxiHours;
+            int maxdays = 20;
+            int maxhours = 100;
+            int Totalwork = 0;
+            int Monthwork = 0;
+            int wagePerHour = 20;
 
             while ((emphours <= maxhours) && (days <= maxdays))
             {
-
-                int atten = EmployeeAttendance.checkatt(Employeee);
+                Random random = new Random();
+                int atten = random.Next(3);
+               
 
                 if (atten == 1)
                 {
+                    Totalwork = wagePerHour * 8;
                     workingHours = 8;
                 }
                 else if (atten == 2)
                 {
+                    Totalwork = wagePerHour * 4;
                     workingHours = 4;
                 }
                 else
                 {
+                    Totalwork = 0;
                     workingHours = 0;
                 }
                 emphours += workingHours;
-                if(emphours <= maxhours)
-                Console.WriteLine("day::{0} employee_hour::{1}", days, emphours);
+                Monthwork += Totalwork;
+                if (emphours <= maxhours)
+                Console.WriteLine("day::{0} employee_hour::{1} empwage : {2}", days, emphours,Totalwork);
                 days++;
             }
-
+            Console.WriteLine("TOTAL MONTH WORK: " + Monthwork);
         }
     }
 }
